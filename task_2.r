17beb2710d96e2b1db88f66ae719ba9160477a49
Sahand Khodabandehloo <sahandkhoda@gmail.com>
2025-09-11 15:11:12 +0200

    Update task_2.r
    Changed from mean to geometric mean. Updated names.

--- task_2.r
+++ task_2.r
@@ -16,20 +16,20 @@ summary(data)
 contr <-  data$NFL[data$GROUP == 0]
 treat <- data$NFL[data$GROUP == 1]
 
-# Get means
-mean_contr <- mean(contr)
-mean_treat <- mean(treat)
+# Get geometric means
+geometric_mean_contr <- exp(mean(contr))
+geometric_mean_treat <- exp(mean(treat))
 
 # Standard deviations
 sd_contr <- sd(contr)
 sd_treat <- sd(treat)
 
 # We want to detect a difference of 30% reduction in the geometric mean of NfL.
-mean_treat <- mean_treat * 0.7
+geometric_mean_treat <- geometric_mean_treat * 0.7
 
 # Calculate Cohen's D effect size
 sd_pooled <- sqrt( (sd_treat^2 + sd_contr^2) / 2)
-es_d <- (mean_treat - mean_contr) / sd_pooled
+es_d <- (geometric_mean_treat - geometric_mean_contr) / sd_pooled
 
 # calculate power over sample size
 p.t.two <- pwr.t.test(d = es_d, power = 0.80, type = "two.sample", 
@@ -46,15 +46,15 @@ my_dataframe <- read.csv("data/my_dataframe.csv")
 head(my_dataframe)
 summary(my_dataframe)
 
-contr2 <- my_dataframe$NFL[my_dataframe$GROUP == 0]
+contr_after <- my_dataframe$NFL[my_dataframe$GROUP == 0]
 treat_after <- my_dataframe$NFL[my_dataframe$GROUP == 1]
 
 # Calculate reduction
 log_before <- log(treat)
 log_after <-  log(treat_after)
 
-geometric_mean <- exp(mean(log_after) - mean(log_before))
-percent_change <- (geometric_mean - 1) * 100
+geometric_mean_dif <- exp(mean(log_after) - mean(log_before))
+percent_change <- (geometric_mean_dif - 1) * 100
 
 # Visualise data
 hist(treat, main = "Before 3 months")

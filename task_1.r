d651e5e5fe6ea52980acc2a30c6ea2601797199a
ellacee <ella.green.se@gmail.com>
2025-09-11 12:44:12 +0200

    Added fitted distributions for P-tau

--- task_1.r
+++ task_1.r
@@ -17,19 +17,24 @@ data <- read.csv("data/Data_T1.csv")
 head(data)
 summary(data)
 
-
-# Visualise data
-hist(data$NFL[data$GROUP == 0], main = "NfL - group 0", xlab = "X")
-hist(data$NFL[data$GROUP == 1], main = "NfL - group 1", xlab = "X")
-
-# Fit the NFL data to the normal distribution
-normal_params_0 <- fitdistr(data$NFL[data$GROUP == 0], "normal")
-normal_params_1 <- fitdistr(data$NFL[data$GROUP == 1], "normal")
-normal_params_0      # Added for visualisation
-normal_params_1      # Added for visualisation
-
 NFL_group_0 = data$NFL[data$GROUP == 0]
 NFL_group_1 = data$NFL[data$GROUP == 1]
+
+PTAU181_group_0 = data$PTAU181[data$GROUP == 0]
+PTAU181_group_1 = data$PTAU181[data$GROUP == 1]
+
+
+# ------------------------- Visualise NfL -------------------------------------
+
+# Histograms with counts
+hist(NFL_group_0, main = "NfL - group 0", xlab = "X")
+hist(NFL_group_1, main = "NfL - group 1", xlab = "X")
+
+# Fit the NFL data to the normal distribution
+normal_params_0 <- fitdistr(NFL_group_0, "normal")
+normal_params_1 <- fitdistr(NFL_group_1, "normal")
+normal_params_0      # Added for visualisation
+normal_params_1      # Added for visualisation
 
 # Do fitted distributions for group 0 
 height_min <- min(NFL_group_0)
@@ -74,11 +79,64 @@ lines(x_pdf, y_weib, col="orange")
 legend(x = "topleft", c("normal","Weibull"), col=c("purple","orange"), lty = 1, cex = 1)
 
 
+# ------------------------- Visualise P-tau181 -------------------------------------
 
-hist(data$PTAU181[data$GROUP == 0], main = "P-tau181 - group 0", xlab = "X")
-hist(data$PTAU181[data$GROUP == 1], main = "P-tau181 - group 1", xlab = "X")
+# Histograms with counts
+hist(PTAU181_group_0, main = "P-tau181 - group 0", xlab = "X")
+hist(PTAU181_group_1, main = "P-tau181 - group 1", xlab = "X")
 
-#boxplot(data$NFL[data$GROUP == 0], data$NFL[data$GROUP == 1], data = data, main = "NfL - both groups")
+# Fit the P-tau181 data to the normal distribution
+normal_params_0 <- fitdistr(PTAU181_group_0, "normal")
+normal_params_1 <- fitdistr(PTAU181_group_1, "normal")
+normal_params_0      # Added for visualisation
+normal_params_1      # Added for visualisation
+
+# Do fitted distributions for group 0 
+height_min <- min(PTAU181_group_0)
+height_max <- max(PTAU181_group_0)
+x_pdf <- seq(height_min, height_max, length.out = 100)
+
+
+weibull_params_0 <- fitdistr(PTAU181_group_0, "weibull")
+# Then we sample from the pdfs using the parameter estimates
+y_norm <- dnorm(x_pdf,
+                mean = normal_params_0$estimate["mean"],
+                sd   = normal_params_0$estimate["sd"])
+
+y_weib <- dweibull(x_pdf,
+                   shape = weibull_params_0$estimate["shape"],
+                   scale = weibull_params_0$estimate["scale"])
+
+# Plot the histogram and lines
+hist(PTAU181_group_0, freq = FALSE, xlab = "P-tau181 concentration", ylab = "Density", main = "P-tau181 Group 0 Fitted Distributions")
+lines(x_pdf, y_norm, col="blue")
+lines(x_pdf, y_weib, col="red")
+legend(x = "topleft", c("normal","Weibull"), col=c("blue","red"), lty = 1, cex = 1)
+
+# Do fitted distributions for group 1 
+height_min <- min(PTAU181_group_1)
+height_max <- max(PTAU181_group_1)
+x_pdf <- seq(height_min, height_max, length.out = 100)
+
+install.packages("fitdistrplus")
+library(fitdistrplus)
+weibull_params_1 <- fitdist(PTAU181_group_1, "weibull")
+# Then we sample from the pdfs using the parameter estimates
+y_norm <- dnorm(x_pdf, 
+                normal_params_1$estimate["mean"], 
+                normal_params_1$estimate["sd"])
+y_weib <- dweibull(x_pdf, 
+                   weibull_params_1$estimate["shape"], 
+                   weibull_params_1$estimate["scale"])
+
+
+# Plot the histogram and lines
+hist(PTAU181_group_1, freq = FALSE, xlab = "P-tau181 concentration", ylab = "Density", main = "P-tau181 Group 1 Fitted Distributions")
+lines(x_pdf, y_norm, col="purple")
+lines(x_pdf, y_weib, col="orange")
+legend(x = "topleft", c("normal","Weibull"), col=c("purple","orange"), lty = 1, cex = 1)
+
+
 
 boxplot(NFL ~ GROUP, data = data, main = "NfL - both groups")
 boxplot(PTAU181 ~ GROUP, data = data, main = "P-tau181 - both groups")

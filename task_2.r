eb20d3efbf3dd5a2c08c11890f372a4155c39474
ellacee <ella.green.se@gmail.com>
2025-09-11 11:03:44 +0200

    Update task_2.r

--- task_2.r
+++ task_2.r
@@ -1,39 +1,63 @@
-# Clozapine-induced tachycardia
-# According to Persson et al. 2024, heart rate as bpm is 
-# approximately normal
-# The mean (sd) heart rate is 73 (9) bpm. Under that assumption we 
-# can sample from the theorical distribution.
-# Normal sampling
 library(pwrss)
 library(pwr)
 install.packages(pwr)
 data <- read.csv("data/Data_T1.csv")
 
 head(data)
-# get mean
 
-x_NFL <- mean(data$NFL)
+contr <-  data$NFL[data$GROUP == 0]
+treat <- data$NFL[data$GROUP == 1]
 
-# Function for geometric mean
-geometric_mean_base <- exp(mean(log(data$NFL)))
+# Get means
+mean_contr <- mean(contr)
+mean_treat <- mean(treat)
 
-sd_contr <- sd(data$NFL)
-nc_samples <- length(data$NFL)
-NFL_contr <- rnorm(nc_samples, x_NFL, sd_NFL)
+# Standard deviations
+sd_contr <- sd(contr)
+sd_treat <- sd(treat)
 
 # We want to detect a difference of 30% reduction in the geometric mean of NfL.
-x_treat <- x_NFL *0.7
-sd_treat <- sd_NFL
-# Data is continuous and approximately normal, suggesting a t-test. 
-# Tachycardia means a bpm of 100, but not the whole day.
-# For simplicity, let's say we want to detect a difference 10 bpm.
-x_treat <- 83
-sd_treat <- 9
-# calculate Cohen's D effect size
-sd_pooled <- sqrt((sd_treat^2+sd_contr^2)/2)
-es_d <- (x_treat- x_contr)/sd_pooled
+mean_treat <- mean_treat * 0.7
+
+nc_samples <- length(data$NFL)
+NFL_contr <- rnorm(nc_samples, mean_contr, sd_contr)
+
+# Calculate Cohen's D effect size
+sd_pooled <- sqrt( (sd_treat^2 + sd_contr^2) / 2)
+es_d <- (mean_treat - mean_contr) / sd_pooled
 
 # calculate power over sample size
 p.t.two <- pwr.t.test(d = es_d, power = 0.80, type = "two.sample", 
                       alternative = "two.sided")
 plot(p.t.two, xlab = "sample size per group")
+
+library(devtools)
+install_github("adamdarwichkth/CM2018rpackage")
+library(CM2018rpackage)
+# run the data retrieval function to load data as dataframe, 
+# specifying the number of study participants per study arm (=N)
+my_dataframe <- ad_trial_data(n_per_arm = 89)
+
+contr2 <- my_dataframe$NFL[my_dataframe$GROUP == 0]
+treat_after <- my_dataframe$NFL[my_dataframe$GROUP == 1]
+
+# Visualise data
+hist(treat, main = "Before 3 months")
+hist(treat_after, main = "After 3 months")
+
+boxplot(treat, treat_after, names = c("Before", "After"), main = "NfL - before and after")
+
+# Test for normality
+shapiro.test(treat)
+shapiro.test(treat_after)
+
+# Calculate reduction
+mean_before <- mean(treat)
+mean_after <-  mean(treat_after)
+
+geometric_mean <- exp(log(mean_after) - log(mean_before))
+
+percent_change <- (geometric_mean - 1) * 100
+
+
+

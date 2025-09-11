bdc73e3cb9e330dec6d4d5337c70cf7cd2cdb0ca
ellacee <ella.green.se@gmail.com>
2025-09-11 14:22:55 +0200

    Update task_2.r

--- task_2.r
+++ task_2.r
@@ -19,9 +19,6 @@ sd_treat <- sd(treat)
 # We want to detect a difference of 30% reduction in the geometric mean of NfL.
 mean_treat <- mean_treat * 0.7
 
-nc_samples <- length(data$NFL)
-NFL_contr <- rnorm(nc_samples, mean_contr, sd_contr)
-
 # Calculate Cohen's D effect size
 sd_pooled <- sqrt( (sd_treat^2 + sd_contr^2) / 2)
 es_d <- (mean_treat - mean_contr) / sd_pooled
@@ -31,11 +28,9 @@ p.t.two <- pwr.t.test(d = es_d, power = 0.80, type = "two.sample",
                       alternative = "two.sided")
 plot(p.t.two, xlab = "sample size per group")
 
-library(devtools)
-install_github("adamdarwichkth/CM2018rpackage")
-library(CM2018rpackage)
-# run the data retrieval function to load data as dataframe, 
-# specifying the number of study participants per study arm (=N)
+# library(devtools)
+# install_github("adamdarwichkth/CM2018rpackage")
+# library(CM2018rpackage)
 
 #my_dataframe <- ad_trial_data(n_per_arm = 89)
 my_dataframe <- read.csv("data/my_dataframe.csv")
@@ -46,18 +41,18 @@ summary(my_dataframe)
 contr2 <- my_dataframe$NFL[my_dataframe$GROUP == 0]
 treat_after <- my_dataframe$NFL[my_dataframe$GROUP == 1]
 
-# Visualise data
-hist(treat, main = "Before 3 months")
-hist(treat_after, main = "After 3 months")
-
-boxplot(treat, treat_after, names = c("Before", "After"), main = "NfL - before and after")
-
 # Calculate reduction
 log_before <- log(treat)
 log_after <-  log(treat_after)
 
 geometric_mean <- exp(mean(log_after) - mean(log_before))
 percent_change <- (geometric_mean - 1) * 100
+
+# Visualise data
+hist(treat, main = "Before 3 months")
+hist(treat_after, main = "After 3 months")
+
+boxplot(treat, treat_after, names = c("Before", "After"), main = "NfL - before and after")
 
 # Test for normality
 shapiro.test(treat)

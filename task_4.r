a169004d218d479844988b50657127918589f7c5
Katrine <katrine.risto@gmail.com>
2025-09-11 15:27:40 +0200

    Update task_4.r

--- task_4.r
+++ task_4.r
@@ -4,9 +4,13 @@ library(ggplot2)
 library(scales)
 library(ggplot2)
 
-# Read The T4 data
+# Read The T4 and T3 data
 data_t4a <- read.csv("data/Data_T4A.csv")
+
+data_T3 <- read.csv("data/Data_T3.csv")
+
 head(data_t4a)
+
 
 #cleaning up the data so there is no commas or space
 data_t4a <- janitor::clean_names(data_t4a)
@@ -19,6 +23,8 @@ colnames(data_t4a)
 df_t4a <- data_t4a %>%
   mutate(response = factor(response,levels = c("Normal", "Mild", "Moderate", "Severe")))
 
+#With the SPMSQ raw data we can show it in a gg plot to easily show the proportions of 
+#the different treatments (control, low, medium and high)
 #gg-plot in percentage 
 ggplot(df_t4a, aes(x = factor(treatment), fill = response)) +
   geom_bar(position = "fill") + scale_y_continuous(labels = scales:: percent) +
@@ -29,3 +35,29 @@ ggplot(df_t4a, aes(x = factor(treatment), fill = response)) +
     title = "SPMSQ Distribution by Treatment Arm"
   ) 
   theme_minimal()
+  
+  
+#Given the biomarkers from previous data in Task3 we now can find out the relation
+#between the biomarkers and the cognitive impairment categories
+  
+#Merging the SPMSQ data with the NFL data into one
+  #use the df_t4a instead of raw data to have order of the categories in the merge
+  
+  
+merged_data <- merge(data_T3, df_t4a, by.x = "ID", by.y = "id")   
+#Boxplot to se the relation
+  
+library(ggplot2)
+
+ggplot(merged_data, aes(x = response, y = NFL, fill = response)) +
+  geom_boxplot(alpha = 0.7) +
+  geom_jitter(width = 0.2, alpha = 0.5) +
+  labs(title = "NFL biomarker levels by SPMSQ category",
+       x = "SPMSQ Category",
+       y = "NFL concentration") +
+  theme_minimal() +
+  theme(legend.position = "none")
+  
+  
+  
+  

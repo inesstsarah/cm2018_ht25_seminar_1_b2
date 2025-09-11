51cefbbf06d09c39d73bf4bf58accdd2ae2b59e5
Sahand Khodabandehloo <sahandkhoda@gmail.com>
2025-09-11 11:51:42 +0200

    Update task_1.r
    Fixed the code on line 60 and 63

--- task_1.r
+++ task_1.r
@@ -55,8 +55,12 @@ x_pdf <- seq(height_min, height_max, 1)
 
 weibull_params_1 <- fitdistr(NFL_group_1, "weibull")
 # Then we sample from the pdfs using the parameter estimates
-y_norm <- dnorm(x_pdf, normal_params_1$estimate["mean"], normal_params$estimate["sd"])
-y_weib <- dweibull(x_pdf, weibull_params_1$estimate["shape"], weibull_params$estimate["scale"])
+y_norm <- dnorm(x_pdf, 
+                normal_params_1$estimate["mean"], 
+                normal_params_1$estimate["sd"])           # Fixed by adding "_1"
+y_weib <- dweibull(x_pdf, 
+                   weibull_params_1$estimate["shape"], 
+                   weibull_params_1$estimate["scale"])    # Fixed by adding "_1"
 
 
 # Plot the histogram and lines
